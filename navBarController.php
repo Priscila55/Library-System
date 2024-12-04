8df05da78318c41811c1ecf4e4db75e328676ccf
Priscila Pirtac <117281138+Priscila55@users.noreply.github.com>
2024-12-04 22:58:34 +0000

    Update navBarController.php

--- navBarController.php
+++ navBarController.php
@@ -1,3 +1,9 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,27 +14,29 @@
 </head>
 <body>
 
-<!--Button to open the sidebar -->
+<!-- Button to open the sidebar -->
 <button onclick="openNav()" class="openBtn">☰</button>
 
 <!-- Navbar -->
 <div id="navBarController" class="sideNav">
-<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <a href="mainPage.php" class="mainPage-link">Home</a>
-    <a href="loginForm.php" class="loginForm-link">Login</a>
-    <a href="register.php" class="register-link">Register</a>
-    <a href="reserveBook.php" class="reserve_book-link">Reserve a Book</a>
+    <!-- Show Login and Register links if the user is not logged in -->
+    <?php if (!isset($_SESSION['user'])): ?>
+        <a href="loginForm.php" class="loginForm-link">Login</a>
+        <a href="register.php" class="register-link">Register</a>
+    <?php endif; ?>
+    
     <a href="myReservations.php" class="myReservations-link">Reservations</a>
+    <a href="books.php" class="books-link">Books</a>
 
-<?php
-   if ( isset($_SESSION['user'])): ?>
-   <form method = "POST" action = "logout.php">
-    <button type = "submit" class = "logout-btn">Log Out</button>
-   </form>
-   <?php endif; ?>
-   </div>
+    <!-- Show Logout link if the user is logged in -->
+    <?php if (isset($_SESSION['user'])): ?>
+        <a href="logout.php" class="logout-link">Log Out</a>
+    <?php endif; ?>
+</div>
 
-<!--JavaScript for opening and closing the sidebar -->
+<!-- JavaScript for opening and closing the sidebar -->
 <script>
     // Function to open the sidebar
     function openNav() {
